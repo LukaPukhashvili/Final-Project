2e16f77da3227509c7978a63e1ec6d1ef473975c
LukaPukhashvili <luka2006.01.22@gmail.com>
2022-01-15 21:23:54 +0400

    add everything

---
+++ final project/script.js
@@ -0,0 +1,14 @@
+
+
+function dosomething(){
+    swal("hurry up!", "this is our number: 574 00 63 77");
+}
+function order(){
+    swal("Good job!", "you have ordered", "success");
+}
+function suborderbuttons(){
+    swal("Nice Choice!", "your order is on the way", "success");
+}
+function feedback(){
+    swal("Feedback was sent successfully!", "We will definitely share your feedback", "success");
+}
